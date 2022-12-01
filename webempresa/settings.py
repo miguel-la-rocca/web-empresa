e02eff633f14344fb2c01c7b2825eb4db4f75bca
miguel-la-rocca <ing.miguellarocca@gmail.com>
2022-12-01 19:43:54 -0300

    app_social_processor

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'services.apps.ServicesConfig',
-    'blog'
+    'blog',
+    'social.apps.SocialConfig',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social.processors.ctx_dict',
             ],
         },
     },
